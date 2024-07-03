f3eb0874a4281f91dc7d8ac0f2c8d5b29f0150e1
cptspacemanspiff <19273992+cptspacemanspiff@users.noreply.github.com>
2024-07-03 13:23:20 -0700

    Added build config parameter for external builds
    can only have one config b/c it is really generating an install artifact.

--- cmake/ExternalBuild.cmake
+++ cmake/ExternalBuild.cmake
@@ -13,7 +13,7 @@ function(cmt_externalbuild_cmake NAME)
     set(multiValueArgs CONFIGURATION_ARGS)
     cmake_parse_arguments("CMTFCN"
         ""
-        ""
+        "BUILD_CONFIG"
         "${multiValueArgs}"
         ${ARGN})
 
@@ -22,23 +22,45 @@ function(cmt_externalbuild_cmake NAME)
     string(TOLOWER ${NAME} NAME_LOWERCASE)
     FetchContent_Populate(${NAME})
 
+    if(NOT DEFINED CMTFCN_BUILD_CONFIG)
+        set(CMTFCN_BUILD_CONFIG "Release")
+    endif()
+
+    set(CMTFCN_EXTRA_CONFIGURE_ARGS "")
+    set(CMTFCN_EXTRA_BUILD_ARGS "")
+    set(CMTFCN_EXTRA_INSTALL_ARGS "")
+
+    if(NOT ${GENERATOR_IS_MULTI_CONFIG})
+        set(CMTFCN_EXTRA_CONFIGURE_ARGS "-DCMAKE_BUILD_TYPE=${CMTFCN_BUILD_CONFIG}")
+    else()
+        set(CMTFCN_EXTRA_BUILD_ARGS "--config;${CMTFCN_BUILD_CONFIG}")
+        set(CMTFCN_EXTRA_INSTALL_ARGS "--config;${CMTFCN_BUILD_CONFIG}")
+    endif()
+
     if(${NAME_LOWERCASE}_POPULATED)
         message(DEBUG "cmt_externalbuild of $NAME cmake build with config args: [${CMTFCN_CONFIGURATION_ARGS}]")
         execute_process(
-            COMMAND ${CMAKE_COMMAND} -S ${${NAME_LOWERCASE}_SOURCE_DIR} -B ${${NAME_LOWERCASE}_BINARY_DIR} ${CMTFCN_CONFIGURATION_ARGS}
+            COMMAND ${CMAKE_COMMAND} -G ${CMAKE_GENERATOR}
+            -S ${${NAME_LOWERCASE}_SOURCE_DIR}
+            -B ${${NAME_LOWERCASE}_BINARY_DIR}
+            ${CMTFCN_EXTRA_CONFIGURE_ARGS}
+            ${CMTFCN_CONFIGURATION_ARGS}
             WORKING_DIRECTORY ${${NAME_LOWERCASE}_BINARY_DIR}
             COMMAND_ECHO STDOUT
             COMMAND_ERROR_IS_FATAL ANY
         )
         ProcessorCount(NCores)
         execute_process(
-            COMMAND ${CMAKE_COMMAND} --build ${${NAME_LOWERCASE}_BINARY_DIR} -j ${NCores}
+            COMMAND ${CMAKE_COMMAND} --build ${${NAME_LOWERCASE}_BINARY_DIR} 
+            -j ${NCores} 
+            ${CMTFCN_EXTRA_BUILD_ARGS}
             WORKING_DIRECTORY ${${NAME_LOWERCASE}_BINARY_DIR}
             COMMAND_ECHO STDOUT
             COMMAND_ERROR_IS_FATAL ANY
         )
         execute_process(
             COMMAND ${CMAKE_COMMAND} --install ${${NAME_LOWERCASE}_BINARY_DIR}
+            ${CMTFCN_EXTRA_INSTALL_ARGS}
             WORKING_DIRECTORY ${${NAME_LOWERCASE}_BINARY_DIR}
             COMMAND_ECHO STDOUT
             COMMAND_ERROR_IS_FATAL ANY
